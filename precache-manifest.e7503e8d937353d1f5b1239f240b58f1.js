78c8f150778c5f541aa23b749867fcc0a64fe97f
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-24 22:45:07 +0200

    Updates

---
+++ precache-manifest.e7503e8d937353d1f5b1239f240b58f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9148878808f4046eee1e2b2f780ea96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "110e29df2ea6c8b5ceeb",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "04b885bc5ccec2dab723",
+    "url": "/static/js/2.28ea1494.chunk.js"
+  },
+  {
+    "revision": "3ef3ac6eb007e8e1078c659a5c07d92e",
+    "url": "/static/js/2.28ea1494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110e29df2ea6c8b5ceeb",
+    "url": "/static/js/main.764a1a19.chunk.js"
+  },
+  {
+    "revision": "10d8eb94bb2d610655e4",
+    "url": "/static/js/runtime-main.6356eff2.js"
+  },
+  {
+    "revision": "c37d9ccf86f7e6ab776107f8fe5a82df",
+    "url": "/static/media/bonecrack.c37d9ccf.wav"
+  },
+  {
+    "revision": "23af77c20e0080d5b56ec2b4929278a2",
+    "url": "/static/media/fireworks.23af77c2.wav"
+  },
+  {
+    "revision": "3497fc944d89a260bc90bf48828d55ac",
+    "url": "/static/media/footsteps.3497fc94.wav"
+  },
+  {
+    "revision": "4a6e1b51a1196334bb380329bb683842",
+    "url": "/static/media/windandchimes.4a6e1b51.wav"
+  }
+]);

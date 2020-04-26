16b123d81c79d3a96d0504fcb1de588a72e866a4
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-26 19:44:20 +0200

    made it work on the mobile

--- src/components/2DPad.js
+++ src/components/2DPad.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import client from "../mqtt";
+import { useGesture } from "react-use-gesture";
 
 import {
   instruments,
@@ -37,9 +38,53 @@ const scale = [
 ];
 
 export default ({ active, type, prefix }) => {
+  const getPosition = (state) => {
+    const touch = state.event.touches[0];
+    const target = state.event.srcElement;
+    if (touch && target) {
+      const x =
+        (touch.clientX - target.getBoundingClientRect().left) /
+        target.offsetWidth;
+      const y =
+        1 -
+        (touch.clientY - target.getBoundingClientRect().top) /
+          target.offsetHeight;
+      console.log(x, y);
+      return { x, y };
+    }
+  };
   const [mouseDown, setMouseDown] = useState(false);
+  const bind = useGesture({
+    onDrag: (state) => {
+      if (!active) return;
+      const pos = getPosition(state);
+      if (pos) {
+        const n = scale[Math.floor(pos.x * scale.length)];
+        client.publish(
+          prefix,
+          JSON.stringify(triggerAttack(type, n, pos.y / 2 + 0.5))
+        );
+      }
+    },
+    onDragStart: (state) => {
+      if (!active) return;
+      const pos = getPosition(state);
+      if (pos) {
+        const n = scale[Math.floor(pos.x * scale.length)];
+        client.publish(
+          prefix,
+          JSON.stringify(triggerAttack(type, n, pos.y / 2 + 0.5))
+        );
+      }
+    },
+    onDragEnd: (state) => {
+      if (!active) return;
+      client.publish(prefix, JSON.stringify(triggerRelease(type)));
+    },
+  });
   return (
     <Container
+      {...bind()}
       onMouseDown={(event) => {
         if (!active) return;
         setMouseDown(true);

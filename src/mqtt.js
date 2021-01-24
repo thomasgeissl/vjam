0f285119f0a61dc0d7fae8488ac51b99f5c9cc4a
Thomas Geissl <thomas.geissl@gmail.com>
2021-01-24 23:09:00 +0100

    updated shiftr broker url

--- src/mqtt.js
+++ src/mqtt.js
@@ -1,4 +1,5 @@
 import mqtt from "mqtt";
 // const client = mqtt.connect("ws://localhost:9001");
-const client = mqtt.connect("wss://try:try@broker.shiftr.io");
+// const client = mqtt.connect("wss://try:try@broker.shiftr.io");
+const client = mqtt.connect("wss://public:public@public.cloud.shiftr.io");
 export default client;
